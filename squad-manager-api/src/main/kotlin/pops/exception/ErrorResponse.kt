34ab7148de9f0956c0b35373c67d2bde2d134fc6
Miguel-Araujo325 <miguel.asilva@sptech.school>
2025-09-12 14:12:19 -0300

    refactor

--- squad-manager-api/src/main/kotlin/pops/exception/ErrorResponse.kt
+++
@@ -1,7 +0,0 @@
-package pops.exception
-
-data class ErrorResponse(
-    val message: String,
-    val status: Int,
-    val path: String
-)
--- squad-manager-api/src/main/kotlin/pops/position/PositionRepository.kt
+++
@@ -1,7 +0,0 @@
-package pops.position
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PositionRepository : JpaRepository<Position, Long> {
-    fun existsByName(name: String): Boolean
-}
